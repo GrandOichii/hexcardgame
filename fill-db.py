24809a5e95eaaf8f77a758a6088b55b734d41130
Igor Yastrebov <i.hawk@yandex.ru>
2023-10-16 18:48:42 +0400

    Finished implementing script for filling db

--- fill-db.py
+++ fill-db.py
@@ -1,12 +1,13 @@
 import json
 from os import path
+import os
 import psycopg2
 import sys
 
 CARDS_DIR = 'cards'
 
 
-def fill_cards():
+def fill():
     cur.execute('''
                 DROP TABLE IF EXISTS Expansions CASCADE ;
                 CREATE TABLE Expansions (
@@ -15,15 +16,14 @@ def fill_cards():
 
                 DROP TABLE IF EXISTS Cards CASCADE ;
                 CREATE TABLE Cards (
-                name VARCHAR NOT NULL PRIMARY KEY,
-                cost INTEGER NOT NULL,
-                type VARCHAR NOT NULL,
-                expansion VARCHAR NOT NULL,
-                text VARCHAR NOT NULL,
-                power INTEGER NOT NULL,
-                life INTEGER NOT NULL,
-                deckUsable BOOLEAN NOT NULL,
-                script TEXT NOT NULL
+                    name VARCHAR NOT NULL PRIMARY KEY,
+                    cost INTEGER NOT NULL,
+                    type VARCHAR NOT NULL,
+                    text VARCHAR NOT NULL,
+                    power INTEGER NOT NULL,
+                    life INTEGER NOT NULL,
+                    deckUsable BOOLEAN NOT NULL,
+                    script TEXT NOT NULL
                 );
 
                 DROP TABLE IF EXISTS ExpansionCards CASCADE ;
@@ -52,29 +52,74 @@ def fill_cards():
                     deckName VARCHAR,
                     FOREIGN KEY (deckName) REFERENCES Decks(name),
 
-                    card INTEGER,
-                    FOREIGN KEY (card) REFERENCES ExpansionCards(id)
+                    cardID INTEGER,
+                    FOREIGN KEY (cardID) REFERENCES ExpansionCards(id)
                 );
 
 ''')
+    # fill cards
     manifest = json.loads(open(path.join(CARDS_DIR, 'manifest.json'), 'r').read())
     expansions = set()
     cards = []
+    
     for card_path in manifest:
         if card_path[0] == '!': continue
         card = json.loads(open(path.join(CARDS_DIR, card_path), 'r').read())
         cards += [card]
         exp = card['expansion']
         expansions.add(exp)
+
     for exp in expansions:
         cur.execute(f"INSERT INTO Expansions VALUES ('{exp}');")
-        print(exp)
-    cur.execute('SELECT * FROM Expansions;')
-    print(cur.fetchall())
+    
+    cid_map = {}
+    for card in cards:
+        power = card['power'] if 'power' in card else -1
+        life = card['life'] if 'life' in card else -1
+        deckUsable = card['deckUsable'] if 'deckUsable' in card else True
+        cur.execute("INSERT INTO Cards VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (card['name'], card['cost'], card['type'], card['text'], power, life, deckUsable, card['script']))
+        cur.execute("INSERT INTO ExpansionCards (expansionName, cardName) VALUES (%s, %s);", (card['expansion'], card['name']))
+        cur.execute("select id from expansioncards where cardName=%s and expansionName=%s;", (card['name'], card['expansion']))
+        id = cur.fetchone()[0]
+        cid_map[card['expansion'] + '::' + card['name']] = id
 
-def fill_decks():
-    # cur.execute('create table decks()')
-    pass
+    # fill decks
+
+    deck_paths = os.listdir('decks')
+    d_i = 0
+    for dp in deck_paths:
+        d_i += 1
+        dp = path.join('decks', dp)
+        if not dp.endswith('.deck'):
+            continue
+        text = open(dp, 'r').read()
+        s = text.split(';')
+
+        # create descriptors
+        d_name = 'deck' + str(d_i)
+        d_description = ''
+        if len(s) > 1:
+            descriptors_raw = s[1]
+            ss = descriptors_raw.split(',')
+            for desc in ss:
+                d_s = desc.split('=')
+                key = d_s[0]
+                value = d_s[1]
+                match key:
+                    case 'name':
+                        d_name = value
+                        break
+                    case 'description':
+                        d_description = value
+        cur.execute("INSERT INTO Decks (name, description) VALUES (%s, %s);", (d_name, d_description))
+        # create card index
+        cards_raw = s[0].split('|')
+        for c_raw in cards_raw:
+            c_s = c_raw.split('#')
+            cid = c_s[0]
+            amount = int(c_s[1])
+            cur.execute("INSERT INTO DeckCards (amount, deckName, cardID) VALUES (%s, %s, %s);", (amount, d_name, cid_map[cid]))
+
 
 DB_NAME = sys.argv[1]
 PASS = sys.argv[2]
@@ -97,7 +142,7 @@ print('Version: ', end='')
 ver = cur.fetchone()
 print(ver)
 
-fill_cards()
+fill()
 conn.commit()
 
 cur.close()

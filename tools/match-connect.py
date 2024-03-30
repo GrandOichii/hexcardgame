4633804edacbe2ac26498bab33f2cd255db01d61
Igor Yastrebov <i.hawk@yandex.ru>
2024-03-30 11:46:48 +0300

    Add scripts for creating and connecting to matches using web sockets

--- tools/match-connect.py
+++ tools/match-connect.py
@@ -1,68 +1,81 @@
 import requests
+import time
 import websocket
 import json
+from sys import argv
 # import rel
 
 BASE_URL = 'http://localhost:5239/api/v1/'
+
+class WebSocketConnection:
+    def force_close(self):
+        self.socket.close()
+
+    def on_open(self, socket: websocket.WebSocket):
+        self.open = True
+        name = input('Enter name: ')
+        # name = 'player1'
+        deck = 'dev::Mana Drill#3|dev::Brute#3|dev::Mage Initiate#3|dev::Warrior Initiate#3|dev::Rogue Initiate#3|dev::Urakshi Shaman#3|dev::Urakshi Raider#3|dev::Give Strength#3|dev::Blood for Knowledge#3|dev::Dragotha Mage#3|dev::Prophecy Scholar#3|dev::Trained Knight#3|dev::Cast Armor#3|dev::Druid Outcast#3|starters::Knowledge Tower#3|dev::Elven Idealist#3|dev::Elven Outcast#3|dev::Barracks#3|dev::Shieldmate#3|dev::Healer Initiate#3|dev::Archdemon Priest#3|dev::Kobold Warrior#3|dev::Kobold Mage#3|dev::Kobold Rogue#3|starters::Dragotha Student#3|starters::Tutoring Sphinx#3|starters::Dragotha Battlemage#3|starters::Inspiration#3'
+        socket.send(
+            json.dumps({
+                'name': name,
+                'deck': deck,
+            })
+        )
+
+    def on_message(self, socket: websocket.WebSocket, message: str):
+        if message == 'ping':
+            socket.send('pong')
+            return
+        if message.startswith('config-'):
+            self.config_received = True
+            return
+        self.first_state_received = True
+        print(message)
+        data = json.loads(message)
+        if data['request'] == 'update':
+            return
+        resp = input('Enter response: ')
+        socket.send(resp)
+        # self.force_close()
+
+    def on_close(self, socket: websocket.WebSocket):
+        self.open = False
+
+    def __init__(self, match_id: str):
+        self.config_received = False
+        self.first_state_received = False
+        self.open = False
+
+        self.socket = websocket.WebSocketApp(WS_URL + 'match/connect/' + match_id,
+                            on_open=self.on_open,
+                            on_message=self.on_message,
+                            on_close=self.on_close)
+        self.socket.run_forever()
+
 WS_URL = 'ws://localhost:5239/api/v1/'
+
 LOGIN_DATA = {
     'username': 'admin',
     'password': 'password',
 }
-CREATE_DATA = json.loads('{"mConfig":"65ffe09e593c39b101fd0b92","canWatch":false,"p1Config":{"botConfig":null},"p2Config":{"botConfig":{"strDeck":"dev::Mana Drill#3|dev::Brute#3|dev::Mage Initiate#3|dev::Warrior Initiate#3|dev::Rogue Initiate#3|dev::Urakshi Shaman#3|dev::Urakshi Raider#3|dev::Give Strength#3|dev::Blood for Knowledge#3|dev::Dragotha Mage#3|dev::Prophecy Scholar#3|dev::Trained Knight#3|dev::Cast Armor#3|dev::Druid Outcast#3|starters::Knowledge Tower#3|dev::Elven Idealist#3|dev::Elven Outcast#3|dev::Barracks#3|dev::Shieldmate#3|dev::Healer Initiate#3|dev::Archdemon Priest#3|dev::Kobold Warrior#3|dev::Kobold Mage#3|dev::Kobold Rogue#3|starters::Dragotha Student#3|starters::Tutoring Sphinx#3|starters::Dragotha Battlemage#3|starters::Inspiration#3","name":"bot-player2","botType":0,"actionDelay":0}}}')
+PLAYER_CONFIG = {
+    "botConfig": None
+}
+BOT_CONFIG = {
+    "botConfig":{
+        "strDeck":"dev::Mana Drill#3|dev::Brute#3|dev::Mage Initiate#3|dev::Warrior Initiate#3|dev::Rogue Initiate#3|dev::Urakshi Shaman#3|dev::Urakshi Raider#3|dev::Give Strength#3|dev::Blood for Knowledge#3|dev::Dragotha Mage#3|dev::Prophecy Scholar#3|dev::Trained Knight#3|dev::Cast Armor#3|dev::Druid Outcast#3|starters::Knowledge Tower#3|dev::Elven Idealist#3|dev::Elven Outcast#3|dev::Barracks#3|dev::Shieldmate#3|dev::Healer Initiate#3|dev::Archdemon Priest#3|dev::Kobold Warrior#3|dev::Kobold Mage#3|dev::Kobold Rogue#3|starters::Dragotha Student#3|starters::Tutoring Sphinx#3|starters::Dragotha Battlemage#3|starters::Inspiration#3",
+        "name":"bot-player2",
+        "botType":0,
+        "actionDelay":0}
+    }
+CREATE_DATA = {
+    "mConfig": "65ffe09e593c39b101fd0b92",
+    "canWatch": False,
+    "p1Config": PLAYER_CONFIG,
+    "p2Config": BOT_CONFIG
+}
 
-resp = requests.request('post', BASE_URL + 'auth/login', json=LOGIN_DATA)
-token = resp.json()['token']
+MATCH_ID = argv[1]
 
-headers = {'Authorization': f'Bearer {token}'}
-resp = requests.request('post', BASE_URL + 'match/create', json=CREATE_DATA, headers=headers)
-match_id = resp.json()['id']
-
-def on_open(socket: websocket.WebSocket):
-    name = input('Enter name: ')
-    deck = 'dev::Mana Drill#3|dev::Brute#3|dev::Mage Initiate#3|dev::Warrior Initiate#3|dev::Rogue Initiate#3|dev::Urakshi Shaman#3|dev::Urakshi Raider#3|dev::Give Strength#3|dev::Blood for Knowledge#3|dev::Dragotha Mage#3|dev::Prophecy Scholar#3|dev::Trained Knight#3|dev::Cast Armor#3|dev::Druid Outcast#3|starters::Knowledge Tower#3|dev::Elven Idealist#3|dev::Elven Outcast#3|dev::Barracks#3|dev::Shieldmate#3|dev::Healer Initiate#3|dev::Archdemon Priest#3|dev::Kobold Warrior#3|dev::Kobold Mage#3|dev::Kobold Rogue#3|starters::Dragotha Student#3|starters::Tutoring Sphinx#3|starters::Dragotha Battlemage#3|starters::Inspiration#3'
-    socket.send(
-        json.dumps({
-            'name': name,
-            'deck': deck,
-        })
-    )
-    print('sent data')
-    # socket.close()
-    # exit(0)
-
-def on_message(socket: websocket.WebSocket, message: str):
-    if message == 'ping':
-        print('received check, responding...')
-        socket.send('pong')
-        return
-    print(message)
-    if message.startswith('config-'):
-        return
-    resp = input('Enter response: ')
-    socket.send(resp)
-
-def on_close(socket: websocket.WebSocket):
-    print("Connection closed")
-
-socket = websocket.WebSocketApp(WS_URL + 'match/connect/' + match_id,
-                            on_open=on_open,
-                            on_message=on_message,
-                            on_close=on_close)
-
-socket.run_forever()
-
-# async def connect_to_server():
-#     async with websockets.connect(WS_URL + 'match/connect/' + match_id) as websocket:
-#         response = await websocket.recv()
-#         print(f"Received: {response}")
-#         name = input('Enter name: ')
-#         deck = '"dev::Mana Drill#3|dev::Brute#3|dev::Mage Initiate#3|dev::Warrior Initiate#3|dev::Rogue Initiate#3|dev::Urakshi Shaman#3|dev::Urakshi Raider#3|dev::Give Strength#3|dev::Blood for Knowledge#3|dev::Dragotha Mage#3|dev::Prophecy Scholar#3|dev::Trained Knight#3|dev::Cast Armor#3|dev::Druid Outcast#3|starters::Knowledge Tower#3|dev::Elven Idealist#3|dev::Elven Outcast#3|dev::Barracks#3|dev::Shieldmate#3|dev::Healer Initiate#3|dev::Archdemon Priest#3|dev::Kobold Warrior#3|dev::Kobold Mage#3|dev::Kobold Rogue#3|starters::Dragotha Student#3|starters::Tutoring Sphinx#3|starters::Dragotha Battlemage#3|starters::Inspiration#3'
-#         await websocket.send(
-#             json.dumps({
-#                 'name': name,
-#                 'deck': deck,
-#             })
-#         )
-#         print('sent data')
-# asyncio.get_event_loop().run_until_complete(connect_to_server())
+conn = WebSocketConnection(MATCH_ID)
---
+++ tools/match-create.py
@@ -0,0 +1,84 @@
+import requests
+import time
+import websocket
+import json
+# import rel
+
+BASE_URL = 'http://localhost:5239/api/v1/'
+
+
+WS_URL = 'ws://localhost:5239/api/v1/'
+
+LOGIN_DATA = {
+    'username': 'admin',
+    'password': 'password',
+}
+PLAYER_CONFIG = {
+    "botConfig": None
+}
+BOT_CONFIG = {
+    "botConfig":{
+        "strDeck":"dev::Mana Drill#3|dev::Brute#3|dev::Mage Initiate#3|dev::Warrior Initiate#3|dev::Rogue Initiate#3|dev::Urakshi Shaman#3|dev::Urakshi Raider#3|dev::Give Strength#3|dev::Blood for Knowledge#3|dev::Dragotha Mage#3|dev::Prophecy Scholar#3|dev::Trained Knight#3|dev::Cast Armor#3|dev::Druid Outcast#3|starters::Knowledge Tower#3|dev::Elven Idealist#3|dev::Elven Outcast#3|dev::Barracks#3|dev::Shieldmate#3|dev::Healer Initiate#3|dev::Archdemon Priest#3|dev::Kobold Warrior#3|dev::Kobold Mage#3|dev::Kobold Rogue#3|starters::Dragotha Student#3|starters::Tutoring Sphinx#3|starters::Dragotha Battlemage#3|starters::Inspiration#3",
+        "name":"bot-player2",
+        "botType":0,
+        "actionDelay":0}
+    }
+CREATE_DATA = {
+    "mConfig": "65ffe09e593c39b101fd0b92",
+    "canWatch": False,
+    "p1Config": PLAYER_CONFIG,
+    "p2Config": BOT_CONFIG
+}
+
+AMOUNT = 3
+resp = requests.request('post', BASE_URL + 'auth/login', json=LOGIN_DATA)
+token = resp.json()['token']
+HEADERS = {'Authorization': f'Bearer {token}'}
+
+def create_match(bot_count: int):
+    data = {
+        "mConfig": "65ffe09e593c39b101fd0b92",
+        "canWatch": False,
+        "p1Config": PLAYER_CONFIG,
+        "p2Config": PLAYER_CONFIG
+    }
+
+    resp = requests.request('post', BASE_URL + 'match/create', json=data, headers=HEADERS)
+    return resp.json()['id']
+
+def config_receive_count():
+    result = 0
+    for conn in connections:
+        if conn.config_received:
+            result += 1
+    return result
+    
+def first_state_receive_count():
+    result = 0
+    for conn in connections:
+        if conn.config_received:
+            result += 1
+    return result
+
+connections = []
+
+match_id = create_match(2)
+print(match_id)
+
+# def connect_to_match(match_id: str):
+#     global connections
+#     connection1 = WebSocketConnection(match_id)
+#     # connection2 = WebSocketConnection(match_id)
+#     connections += [connection1]
+#     # connections += [connection2]
+
+# for i in range(AMOUNT):
+#     match_id = create_match(2)
+#     connect_to_match(match_id)
+# while True:
+#     crc = config_receive_count()
+#     gsrc = first_state_receive_count()
+#     print(f'Config receive: {crc}\tFirst state receive: {gsrc}')
+#     if crc == AMOUNT and gsrc == AMOUNT:
+#         break
+#     time.sleep(50)

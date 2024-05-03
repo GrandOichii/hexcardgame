18fb27c4048f15fc5365290f8f0191829057cbdc
Igor Yastrebov <i.hawk@yandex.ru>
2024-05-03 05:50:07 -0400

    Add documentation to extension classes

--- ManagerBack/Extensions/ControllerBaseExtensions.cs
+++ ManagerBack/Extensions/ControllerBaseExtensions.cs
@@ -3,7 +3,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ManagerBack.Extensions;
 
+/// <summary>
+/// Extension class for ControllerBase objects
+/// </summary>
 public static class ControllerBaseExtensions {
+    /// <summary>
+    /// Extracts claim value from the HTTP context claims
+    /// </summary>
+    /// <typeparam name="T">Value type</typeparam>
+    /// <param name="controller">HTTP controller</param>
+    /// <param name="type">Claim type</param>
+    /// <returns>Claim value</returns>
     public static string ExtractClaim<T> (this T controller, string type) where T : ControllerBase {
         var result = controller.HttpContext.User.Claims.FirstOrDefault(c => c.Type == type);
         return result!.Value;
--- ManagerBack/Extensions/WebSocketExtensions.cs
+++ ManagerBack/Extensions/WebSocketExtensions.cs
@@ -3,6 +3,9 @@ using System.Text;
 
 namespace ManagerBack.Extensions;
 
+/// <summary>
+/// General WebSocket exception class
+/// </summary>
 [System.Serializable]
 public class WebSocketClosedException : System.Exception
 {
@@ -10,7 +13,17 @@ public class WebSocketClosedException : System.Exception
     public WebSocketClosedException(string message) : base(message) { }
 }
 
+/// <summary>
+/// Extension class for WebSocket objects
+/// </summary>
 public static class WebSocketExtensions {
+    /// <summary>
+    /// Reads the information from the WebSocket
+    /// </summary>
+    /// <param name="socket">WebSocket object</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns>Read data as a UTF8 string</returns>
+    /// <exception cref="WebSocketClosedException"></exception>
     public static async Task<string> Read(this WebSocket socket, CancellationToken cancellationToken) {
         if (socket.State == WebSocketState.Closed)
             throw new WebSocketClosedException("socket is closed");
@@ -24,15 +37,33 @@ public static class WebSocketExtensions {
         return Encoding.UTF8.GetString(buffer).Replace("\0", string.Empty);
     }
 
+    /// <summary>
+    /// Reads the information from the WebSocket, uses empty cancellation token
+    /// </summary>
+    /// <param name="socket">WebSocket object</param>
+    /// <returns>Read data as a UTF8 string</returns>
     public static async Task<string> Read(this WebSocket socket) {
         return await Read(socket, CancellationToken.None);
     }
 
-
+    /// <summary>
+    /// Writes the string data to the socket, uses empty cancellation token
+    /// </summary>
+    /// <param name="socket">WebSocket object</param>
+    /// <param name="message">Message</param>
+    /// <returns></returns>
     public static async Task Write(this WebSocket socket, string message) {
         var serverMsg = Encoding.UTF8.GetBytes(message);
         await socket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), WebSocketMessageType.Text, true, CancellationToken.None);
     }
+
+    /// <summary>
+    /// Writes the string data to the socket, uses empty cancellation token
+    /// </summary>
+    /// <param name="socket">WebSocket object</param>
+    /// <param name="message">Message</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <returns></returns>
     public static async Task Write(this WebSocket socket, string message, CancellationToken cancellationToken) {
         var serverMsg = Encoding.UTF8.GetBytes(message);
         await socket.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), WebSocketMessageType.Text, true, cancellationToken);
